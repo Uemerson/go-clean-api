f2fa2d72b9babddcbbb7919cc954c460a36e6f45
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-05-06 00:28:12 -0300

    chore: add bash scripts

--- up.dev.v2.sh
+++ up.dev.v2.sh
@@ -10,4 +10,5 @@ function trap_ctrlc ()
 }
 
 trap "trap_ctrlc" 2
-docker compose -f docker-compose.yml --env-file .env up -d --build --remove-orphans
+docker compose -f docker-compose.dev.yml --env-file .env.dev up -d --build --remove-orphans
+docker compose -f docker-compose.dev.yml logs -f --tail=15 api
