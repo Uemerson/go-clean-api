ab2565f41ec30a964dde3b37570b7aed454ae689
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-05-12 15:53:45 -0300

    refactor: user handler

--- internal/application/user/handler/handler.go
+++ internal/application/user/handler/handler.go
@@ -1,10 +1,6 @@
 package handler
 
 import (
-	"encoding/json"
-	"net/http"
-
-	"github.com/Uemerson/clean-go-api/internal/configuration/exception"
 	"github.com/Uemerson/clean-go-api/internal/domain/user/contracts"
 )
 
@@ -14,31 +10,4 @@ type UserHandler struct {
 
 func NewUserHandler(s contracts.Service) *UserHandler {
 	return &UserHandler{s: s}
-}
-
-type UserRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func (uh *UserHandler) Add(w http.ResponseWriter, r *http.Request) {
-	var ur UserRequest
-	if err := json.NewDecoder(r.Body).Decode(&ur); err != nil {
-		err := exception.InternalServerError()
-		w.WriteHeader(err.StatusCode)
-		json.NewEncoder(w).Encode(err)
-		return
-	}
-	user, err := uh.s.Add(ur.Name, ur.Email, ur.Password)
-	if err != nil {
-		w.WriteHeader(err.StatusCode)
-		json.NewEncoder(w).Encode(err)
-		return
-	}
-	if err := json.NewEncoder(w).Encode(user); err != nil {
-		err := exception.InternalServerError()
-		w.WriteHeader(err.StatusCode)
-		json.NewEncoder(w).Encode(err)
-	}
 }
