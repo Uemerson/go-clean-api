d4205a373e99406bac539183919d509c7651db61
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-05-12 16:04:00 -0300

    feat: add not null to user entity

--- internal/domain/user/infra/repository/entity/entity.go
+++ internal/domain/user/infra/repository/entity/entity.go
@@ -3,7 +3,7 @@ package entity
 import "time"
 
 type User struct {
-	Id        string `gorm:"primaryKey;size:36;"`
+	Id        string `gorm:"primaryKey;not null;size:36;default:null"`
 	Name      string `gorm:"size:255;"`
 	Email     string `gorm:"size:255;index:idx_email,unique"`
 	Password  string `gorm:"size:72;"`
